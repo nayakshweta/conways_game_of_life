e9f37a560e4850706a3630147c75bcdd2d9f4ede
shweta <nayakshweta07@gmail.com>
2019-03-23 15:06:35 +0530

    Adds method to calculate the next status of the cell given the current live neighbour count.

--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from cell import cell
+from cell import Cell
+from coordinate import Coordinate
 
 class TestCell(TestCase):
     def test_cell_initialization(self):
@@ -8,3 +9,27 @@ class TestCell(TestCase):
 
         assert cell.coordinate == coordinate
         assert cell.status == True
+
+    def test_cell_set_next_status_while_live_neighbours_3_and_current_status_live(self):
+        coordinate = Coordinate(2, 2)
+        cell = Cell(coordinate, True)
+        live_neighbour_count = 3
+
+        next_status = cell.calculate_next_status(live_neighbour_count)
+        assert next_status == True
+
+    def test_cell_set_next_status_while_live_neighbours_6_and_current_status_live(self):
+        coordinate = Coordinate(2, 2)
+        cell = Cell(coordinate, True)
+        live_neighbour_count = 6
+
+        next_status = cell.calculate_next_status(live_neighbour_count)
+        assert next_status == False
+
+    def test_cell_set_next_status_while_live_neighbours_3_and_current_status_dead(self):
+        coordinate = Coordinate(2, 2)
+        cell = Cell(coordinate, False)
+        live_neighbour_count = 3
+
+        next_status = cell.calculate_next_status(live_neighbour_count)
+        assert next_status == True
