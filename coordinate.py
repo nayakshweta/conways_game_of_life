23cdc15749019b63d13f058c82303036b12f72d4
shweta <nayakshweta07@gmail.com>
2019-03-23 14:40:26 +0530

    Coordinates need to be comparable to each other. This commit adds the comparion overload methods to the coordinate class.

--- coordinate.py
+++ coordinate.py
@@ -2,3 +2,14 @@ class Coordinate:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if self.x == other.x and self.y == other.y:
+                return True
+        return False
+
+
+    def __ne__(self, other):
+        return not isinstance(other, self.__class__) or (self.x != other.x and self.y != other.y)
--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -7,4 +7,17 @@ class TestCoordinate(TestCase):
         coordinate = Coordinate(1, 2)
         assert coordinate.x == 1
         assert coordinate.y == 2
-        
+
+
+    def test_coordinate_comparison(self):
+        coordinate1 = Coordinate(2, 3)
+        coordinate2 = Coordinate(2, 3)
+
+        assert coordinate1 == coordinate2
+
+
+    def test_coordinate_negative_comparison(self):
+        coordinate1 = Coordinate(1, 2)
+        coordinate2 = Coordinate(2, 3)
+
+        assert coordinate1 != coordinate2
