aee83f2ae7fcee048305c23bde3186e24bdfa0eb
shweta <nayakshweta07@gmail.com>
2019-03-23 15:47:59 +0530

    Adds method to create the output file a grid.

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from grid import Grid
+from coordinate import Coordinate
 
 def get_array_of_strings_from_file(file_path):
     with open('input_file.txt', 'r') as file:
@@ -26,6 +27,30 @@ def create_grid_from_array_of_strings(lines_unformatted):
         r += 1
 
     return grid
+
+def create_output_file_from_grid(next_grid):
+    rows = next_grid.row_count
+    columns = next_grid.column_count
+    cells = next_grid.cells
+    r = 0
+    c = 0
+    f = open('output.txt', 'w')
+    
+    while r < rows:
+        while c < columns:
+            for cell in cells:
+                coordinate = Coordinate(r, c)
+                if cell.coordinate == coordinate:
+                    status = cell.get_current_status()
+            if status == True :
+                f.write('*')
+            elif status == False:
+                f.write('.')
+            c += 1
+        c = 0
+        r += 1
+        f.write('\n')
+    f.close()
 
 if __name__ == '__main__':
     lines = get_array_of_strings_from_file('input_file.txt')
