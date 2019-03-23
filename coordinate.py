cbd3c56e8d65078f6ffead332318712b79fbcdc0
shweta <nayakshweta07@gmail.com>
2019-03-23 14:46:35 +0530

    Adds a method to retrieve a list of neighbouring coordinates for a given coordinate.

--- coordinate.py
+++ coordinate.py
@@ -13,3 +13,33 @@ class Coordinate:
 
     def __ne__(self, other):
         return not isinstance(other, self.__class__) or (self.x != other.x and self.y != other.y)
+
+
+    def get_neighbouring_cordinates(self, grid_row_size, grid_column_size):
+        coordinates = []
+
+        if (self.x - 1) >= 0:
+            coordinates.append(Coordinate(self.x - 1, self.y))
+
+            if (self.y - 1) >= 0:
+                coordinates.append(Coordinate(self.x - 1, self.y - 1))
+            
+            if (self.y + 1) < grid_column_size:
+                coordinates.append(Coordinate(self.x - 1, self.y + 1))
+        
+        if (self.y - 1) >= 0:
+            coordinates.append(Coordinate(self.x, self.y - 1))
+        
+        if (self.y + 1) < grid_column_size:
+            coordinates.append(Coordinate(self.x, self.y + 1))
+
+        if (self.x + 1) < grid_row_size:
+            coordinates.append(Coordinate(self.x + 1, self.y))
+
+            if (self.y - 1) >= 0:
+                coordinates.append(Coordinate(self.x + 1, self.y - 1))
+            
+            if (self.y + 1) < grid_column_size:
+                coordinates.append(Coordinate(self.x + 1, self.y + 1))
+
+        return coordinates
--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -21,3 +21,63 @@ class TestCoordinate(TestCase):
         coordinate2 = Coordinate(2, 3)
 
         assert coordinate1 != coordinate2
+
+
+
+    def test_get_neighbouring_coordinates_3_3(self):
+        coordinate = Coordinate(3, 3)
+        actual_neighbouring_coordinates = coordinate.get_neighbouring_cordinates(6, 6)
+
+        assert len(actual_neighbouring_coordinates) == 8
+
+        assert Coordinate(2, 2) in actual_neighbouring_coordinates
+        assert Coordinate(2, 3) in actual_neighbouring_coordinates
+        assert Coordinate(2, 4) in actual_neighbouring_coordinates
+        assert Coordinate(3, 2) in actual_neighbouring_coordinates
+        assert Coordinate(3, 4) in actual_neighbouring_coordinates
+        assert Coordinate(4, 2) in actual_neighbouring_coordinates
+        assert Coordinate(4, 3) in actual_neighbouring_coordinates
+        assert Coordinate(4, 4) in actual_neighbouring_coordinates
+    
+    def test_get_neighbouring_coordinates_0_0(self):
+        coordinate = Coordinate(0, 0)
+        actual_neighbouring_coordinates = coordinate.get_neighbouring_cordinates(4, 4)
+
+        assert len(actual_neighbouring_coordinates) == 3
+
+        assert Coordinate(0, 1) in actual_neighbouring_coordinates
+        assert Coordinate(1, 0) in actual_neighbouring_coordinates
+        assert Coordinate(1, 1) in actual_neighbouring_coordinates
+
+
+    def test_get_neighbouring_coordinates_4_4(self):
+        coordinate = Coordinate(4, 4)
+        actual_neighbouring_coordinates = coordinate.get_neighbouring_cordinates(4, 4)
+
+        assert len(actual_neighbouring_coordinates) == 3
+
+        assert Coordinate(3, 4) in actual_neighbouring_coordinates
+        assert Coordinate(4, 3) in actual_neighbouring_coordinates
+        assert Coordinate(3, 3) in actual_neighbouring_coordinates
+
+
+    def test_get_neighbouring_coordinates_4_0(self):
+        coordinate = Coordinate(4, 0)
+        actual_neighbouring_coordinates = coordinate.get_neighbouring_cordinates(4, 4)
+
+        assert len(actual_neighbouring_coordinates) == 3
+
+        assert Coordinate(3, 0) in actual_neighbouring_coordinates
+        assert Coordinate(3, 1) in actual_neighbouring_coordinates
+        assert Coordinate(4, 1) in actual_neighbouring_coordinates
+
+
+    def test_get_neighbouring_coordinates_0_4(self):
+        coordinate = Coordinate(0, 4)
+        actual_neighbouring_coordinates = coordinate.get_neighbouring_cordinates(4, 4)
+
+        assert len(actual_neighbouring_coordinates) == 3
+
+        assert Coordinate(0, 3) in actual_neighbouring_coordinates
+        assert Coordinate(1, 3) in actual_neighbouring_coordinates
+        assert Coordinate(1, 4) in actual_neighbouring_coordinates
