048111c01510d10d61efc3ffe71a9b0f91b69097
shweta <nayakshweta07@gmail.com>
2019-03-23 15:20:06 +0530

    Adds method to mark a cell within a grid.

--- grid.py
+++ grid.py
@@ -1,5 +1,13 @@
+from coordinate import Coordinate
+from cell import Cell
+
 class Grid:
     def __init__(self, row_count, column_count):
         self.row_count = row_count
         self.column_count = column_count
         self.cells = []
+
+    def mark_cell(self, x, y, status):
+        coordinate = Coordinate(x, y)
+        cell = Cell(coordinate, status)
+        self.cells.append(cell)
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -9,4 +9,9 @@ class TestGrid(TestCase):
         assert grid.row_count == 4
         assert grid.column_count == 6
         assert grid.cells == []
-        
+
+    def test_grid_mark_cell(self):
+        grid = Grid(4, 6)
+        grid.mark_cell(1, 2, True)
+
+        assert len(grid.cells) == 1
