813ab5cb17b6f04e2092fb08b9035d04c949f763
shweta <nayakshweta07@gmail.com>
2019-03-23 15:34:41 +0530

    Adds main method to take an input file and write the output file. This method calls a bunch of other methods each performing a single task in order to get the final result.

---
+++ conways_game_of_life.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    lines = get_array_of_strings_from_file('input_file.txt')
+    grid = create_grid_from_array_of_strings(lines)
+
+    next_grid = grid.calculate_next_grid()
+    create_output_file_from_grid(next_grid)
